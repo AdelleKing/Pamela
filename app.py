584b563cfb1b18fb035b1699877a81a7066526dd
AdelleKing <adelle_xx@yahoo.co.uk>
2024-04-09 20:36:40 +0100

    all pages nad links working. Styling complete

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
 
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 
 
 app = Flask(__name__) 
+app.config['MAIL_SERVER'] = 'smpt.yahoo.com'
+app.config["MAIL_PORT"] = "465"
+app.config["MAIL_USERNAME"] = "adelle_xx@yahoo.co.uk" 
+app.config["MAIL_PASSWORD"] = os.environ.get('PASSWORD')
+app.config["MAIL_USE_TLS"] = "False"
+app.config["MAIL_USE_SSL"] = "True"
+mail = Mail(app)
   
 @app.route("/")
 def home():
@@ -19,6 +27,13 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+@app.route("/contactform")
+def contactform():
+    return render_template("contactform.html")
 
 
 
