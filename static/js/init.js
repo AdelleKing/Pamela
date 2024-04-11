a5042777a19b77bb2da14f4fc369130625d93412
AdelleKing <adelle_xx@yahoo.co.uk>
2024-04-11 14:47:11 +0100

    adding page redirect

--- static/js/init.js
+++ static/js/init.js
@@ -7,3 +7,32 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+//send the email 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_io15uze';
+   const templateID = 'test';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+      
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+
